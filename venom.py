0f13eb2afc6bd004f54191f873b65308b9037666
nikhil03-bh <nikhildahiya03@gmail.com>
2024-07-03 11:35:35 +0530

    Update venom.py

--- venom.py
+++ venom.py
@@ -18,9 +18,9 @@ loop = asyncio.get_event_loop()
 
 TOKEN = '7251491400:AAEgxiIKUa4kQGjn6_R0J5-rEWlTNC9llpM'
 MONGO_URI = 'mongodb+srv://VENOMxCRAZY:CRAZYxVENOM@cluster0.ythilmw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0&tlsAllowInvalidCertificates=true'
-FORWARD_CHANNEL_ID = -5826987835
-CHANNEL_ID = -5826987835
-error_channel_id = -5826987835
+FORWARD_CHANNEL_ID = -1002188147894
+CHANNEL_ID = -1002188147894
+error_channel_id = -1002188147894
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
